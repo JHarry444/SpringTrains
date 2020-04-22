c506cd071e22aa3518ca8b529bb684a2b27db1f3
Jordan <jordan.harrison@qa.com>
2020-04-22 10:59:41 +0100

    Added autowiring

--- src/main/java/com/qa/demo/SpringExampleApplication.java
+++ src/main/java/com/qa/demo/SpringExampleApplication.java
@@ -6,16 +6,17 @@ import org.springframework.context.ApplicationContext;
 
 import com.qa.demo.data.domain.Train;
 import com.qa.demo.data.repo.TrainRepo;
+import com.qa.demo.services.TrainService;
 
 @SpringBootApplication
 public class SpringExampleApplication {
-	
-	//bean -> managed object
-	//IoC -> inversion of control -> program manages objects for the dev
+
+	// bean -> managed object
+	// IoC -> inversion of control -> program manages objects for the dev
 	public static void main(String[] args) {
 		ApplicationContext ac = SpringApplication.run(SpringExampleApplication.class, args);
-		TrainRepo repo  = ac.getBean(TrainRepo.class);
-		repo.save(new Train("Thomas", "Blue"));
+		TrainService service = ac.getBean(TrainService.class);
+		service.create(new Train("Thomas", "Blue"));
 	}
 
 }
--- src/main/java/com/qa/demo/services/TrainService.java
+++ src/main/java/com/qa/demo/services/TrainService.java
@@ -1,5 +1,23 @@
 package com.qa.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.demo.data.domain.Train;
+import com.qa.demo.data.repo.TrainRepo;
+
+@Service
 public class TrainService {
+
+	
+	private TrainRepo repo;
+
+	public TrainService(TrainRepo repo) {
+		this.repo = repo;
+	}
+	
+	public void create(Train train) {
+		this.repo.save(train);
+	}
 
 }
